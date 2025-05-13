25c6fcbca847e4e003201b662ef37b9ccbcc7d7c
Ritesh672 <riteshyad672@gmail.com>
2025-05-13 10:49:15 +0530

    final commit for the frontend before deploying

--- client/src/api/task.js
+++ client/src/api/task.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
 // This file contains the API calls to the backend for task management.s
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL =  process.env.REACT_APP_API_URL
+
+console.log('▶️ BASE_URL =', process.env.REACT_APP_API_URL);
 
 export const getTasks = () => axios.get(`${BASE_URL}/tasks`);
 export const getTask = (id) => axios.get(`${BASE_URL}/tasks/${id}`);
